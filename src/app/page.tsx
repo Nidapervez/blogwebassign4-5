9649a028142b2ab0a59eec2c759ea2aeec82ccba
Nida <nnpervez333@gmail.com>
2024-10-28 22:13:45 -0700

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,22 @@ import image from "../app/1.webp"; // Import the image from the app folder
 import Navbar from "./components/navbar";
 import Card from "./components/card";
 
-
 export default function Keyframe() {
   return (
-   <div> <Navbar/>
-   
-   <div className={styles.box}>
-  <h4 className="font-bold text-3xl text-center font-mono">Welcome to Blogging Website</h4>
-  <p className="text-2xl text-center font-serif">Read our blogs</p>
-  <div className={styles.img}>
-    <Image src={image} alt="Blog Image" layout="responsive" width={300} height={200} />
-    <div className={styles.content}>
-      <h1>Blogging Site</h1>
-      <p>Join us for daily blog readings</p>
+    <div>
+      <Navbar />
+      <div className={styles.box}>
+        <h4 className="font-bold text-3xl text-center font-mono">Welcome to Blogging Website</h4>
+        <p className="text-2xl text-center font-serif">Read our blogs</p>
+        <div className={styles.img}>
+          <Image src={image} alt="Blog Image" layout="fill" objectFit="cover" />
+          <div className={styles.content}>
+            <h1>Blogging Site</h1>
+            <p>Join us for daily blog readings</p>
+          </div>
+        </div>
+      </div>
+      <Card />
     </div>
-  </div>
-</div>
-  
-    <Card/>
-    </div>);}
+  );
+}
